6b012f773a2f39b61faa5fa73d66521644cde61e
Anakbm505 <ekaterina505km@outlook.com>
2026-07-14 20:43:56 -0400

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -6,3 +6,18 @@ d.floor
 FROM associates a
 JOIN departments d ON a.department_id = d.department_id
 WHERE a.status = 'Active';
+
+Query 2
+
+SELECT
+a.associate_id,
+a.full_name,
+d.dept_name,
+si.incident_date,
+si.severity,
+si.incident_type
+FROM associates a
+JOIN departments d ON a.department_id = d.department_id
+LEFT JOIN safety_incidents si ON a.associate_id = si.associate_id
+WHERE a.status = 'Active'
+ORDER BY a.full_name;   
